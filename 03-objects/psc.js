71ebacb765115e8c9276492e63a4cb0fcfeaa5cc
Robert <bob.e.tanner@gmail.com>
2020-03-26 16:46:33 -0600

    started 130c

--- 03-objects/psc.js
+++ 03-objects/psc.js
@@ -18,5 +18,24 @@
         }
     
     }
+    class AccountController {
 
-export default {Account};
+      constructor(name, balance) {
+          this.name = name;
+          this.balance = balance;
+      }
+  
+      deposit(num) {
+          this.balance = this.balance +num;
+      }
+      withdrawl(num) {
+        this.balance = this.balance -num;
+    }
+      check() {
+    
+          return this.balance
+      }
+  
+  }
+export default {Account, AccountController};
+// export { Account, AccountController };
