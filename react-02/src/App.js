a62a2cc86a6bd16de8a8772c2e3656ddd1c8e3cc
Robert <bob.e.tanner@gmail.com>
2020-06-08 14:46:29 -0600

    On to 140c

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,32 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import spider1 from './components/spider1.svg';
 import spider2 from './components/spider2.svg';
 import spider3 from './components/spider3.svg';
 import spider4 from './components/spider4.svg';
+import Logo from './components/logo.js';
+import Game from './tictactoe/ticTacToe.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-      <div className="spiderContainer" >
-      
-        <div className ="imgContainer"><img src={spider1} className="App-logo spiders" alt="logo" /></div>
-        <div className ="imgContainer"><img src={spider2} className="App-logo spiders" alt="logo" /></div>
-        <div className ="imgContainer"><img src={spider3} className="App-logo spiders" alt="logo" /></div>
-        <div className ="imgContainer"><img src={spider4} className="App-logo spiders" alt="logo" /></div>
-     
-          </div>
-        <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      appToRender: <Game/>,
+    }
+  }
+  logoClick = (parm) => {
 
-      </header>
-    </div>
-  );
+    console.log("onclick ", parm.target.getAttribute("todo"));
+
+    switch (parm.target.getAttribute("todo")) {
+      case "logo":
+        this.setState({ appToRender: <Logo /> });
+        break;
+      case "TicTacToe":
+        this.setState({ appToRender: <Game /> });
+        break;
+      default:
+        this.setState({ appToRender: <Logo /> });
+    }
+
+    // this.setState({ appToRender: <TicTacToe /> });
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+
+        <div className="spiderContainer" >
+
+          <div onClick={this.logoClick} todo="logo" className="imgContainer"><img src={spider1} className="App-logo spiders" alt="logo" /></div>
+          <div onClick={this.logoClick} todo="TicTacToe" className="imgContainer"><img src={spider2} className="App-logo spiders" alt="logo" /></div>
+          <div className="imgContainer"><img src={spider3} className="App-logo spiders" alt="logo" /></div>
+          <div className="imgContainer"><img src={spider4} className="App-logo spiders" alt="logo" /></div>
+
+        </div>
+        {this.state.appToRender}
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-02/src/components/logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const Logo = () => {
+
+    return (
+        <div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+            </header>
+        </div>
+    );
+}
+export default Logo;
