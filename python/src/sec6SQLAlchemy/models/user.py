3ec0e06cd62ce3e4b9cca4ad54c81da6d488d0aa
Robert <bob.e.tanner@gmail.com>
2020-07-28 21:16:18 -0600

    adding Heroku required files for running

--- python/src/sec6SQLAlchemy/models/user.py
+++ python/src/sec6SQLAlchemy/models/user.py
@@ -1,46 +1,25 @@
 import sqlite3
-from flask_restful import Resource, reqparse
 from db import db
 
 class UserModel(db.Model): #This is a model becasue it cannot recieve data or send data.  Models are for internal code
-    __tablename__='users'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self,_id,username, password):
-        self.id= _id
+    def __init__(self,username, password):
         self.username=username
         self.password=password
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
     @classmethod
     def find_by_username(cls,username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query,(username,)) # comma is needed becasue this neds to be a tuple
-        row = result.fetchone() # gets first row
-        if row:
-            user = cls(*row) # *row replaces (row[0],row[1],row[2])
-        else:
-            user = None
-        
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls,_id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query,(_id,)) # comma is needed becasue this neds to be a tuple
-        row = result.fetchone() # gets first row
-        if row:
-            user = cls(*row) # *row replaces (row[0],row[1],row[2])
-        else:
-            user = None
-        
-        connection.close()
-        return user
+       return cls.query.filter_by(id=_id).first()
--- python/src/sec6SQLAlchemy/resources/item.py
+++ python/src/sec6SQLAlchemy/resources/item.py
@@ -1,85 +1,65 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
 
+
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('price',
-        type=float,   
-        required=True,
-        help="This field cannot be left blank"
-        )
-    @jwt_required()
-    def get(self,name):
-        item=ItemModel.find_by_name(name)
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help="Every item needs a store_id."
+                        )
 
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
         if item:
             return item.json()
-        return {'message':'Item not found'}, 404
+        return {'message': 'Item not found'}, 404
 
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
-        
+
         data = Item.parser.parse_args()
 
-        item=ItemModel(name,data["price"])
+        item = ItemModel(name, **data)
 
         try:
-            item.insert()
+            item.save_to_db()
         except:
-            return{"message":"An error ocurred inserting the item."}, 500 #internal server error
+            return {"message": "An error occurred inserting the item."}, 500
 
-
-        return item, 201
-
+        return item.json(), 201
 
     def delete(self, name):
-        
-        connection = sqlite3.connect('data.db')
-        cursor= connection.cursor()
-
-        query = "DELETE FROM items where name=?"
-        cursor.execute(query,(name,))
-
-        connection.commit()
-        connection.close()
-       
-        return {'message': 'Item deleted'}
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return {'message': 'Item deleted.'}
+        return {'message': 'Item not found.'}, 404
 
     def put(self, name):
         data = Item.parser.parse_args()
 
         item = ItemModel.find_by_name(name)
-        updated_item=ItemModel(name, data["price"])
-        if item is None: #prevents duplicate names
-            try:
-                updated_item.insert()
-            except:
-                return{"message":"An error occurred inserting the item."},500
-        else: 
-            try:
-                updated_item.update() #gets updated properties and updates the database
-            except:
-                return{"message":"An error occurred updating the item."},500
-        return updated_item.json, 201
+
+        if item:
+            item.price = data['price']
+        else:
+            item = ItemModel(name, **data)
+
+        item.save_to_db()
+
+        return item.json()
 
 
 class ItemList(Resource):
     def get(self):
-        
-        connection = sqlite3.connect('data.db')
-        cursor= connection.cursor()
-
-        query = "SELECT * FROM items"
-        result =cursor.execute(query)
-
-        connection.commit()
-  
-        items=[]
-        for row in result:
-            items.append({'name':row[0],'price':row[1]})
-
-        connection.close()           
-        return {'item':items}
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
